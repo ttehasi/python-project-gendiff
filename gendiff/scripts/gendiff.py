8a476653093b324f9852699b6e67694e8a8b66ae
ttehasi <tatiana.chudowa@my.com>
2025-01-10 12:34:30 +0300

    changes 1-st step"s

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,15 @@
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog='gendiff',
+        description='''Compares two configuration files and shows a difference.'''
+        )
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    print(parser.parse_args())
+
+
+if __name__ == "__main__":
+    main()
