78c7e5977a631065b3943d3cdc18bc95b35d3fe2
ttehasi <tatiana.chudowa@my.com>
2025-01-11 12:23:16 +0300

    add --format/-f(changes 2nd step)

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -8,6 +8,7 @@ def main():
         )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
     print(parser.parse_args())
 
 
